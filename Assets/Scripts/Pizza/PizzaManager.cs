f6a17c3e2b39fb529ddf6fad7a0860ab5f40e895
Romain Unternaehrer <r.unternaehrer@rubika-edu.com>
2026-09-24 17:03:07 +0200

    Fill and empty trays

--- Assets/Scripts/Pizza/PizzaManager.cs
+++ Assets/Scripts/Pizza/PizzaManager.cs
@@ -5,8 +5,6 @@ using UnityEngine.InputSystem;
 using UnityEngine.SocialPlatforms.Impl;
 using UnityEngine.UI;
 
-
-
 public class PizzaManager : MonoBehaviour
 {
     public enum PizzaState
@@ -15,6 +13,13 @@ public class PizzaManager : MonoBehaviour
         IngredientAssembly,
         Cooking,
         Ready
+    }
+
+    [System.Serializable]
+    public struct IngredientTray
+    {
+        public SSO_Ingredient Ingredient;
+        public GameObject FilledVisual;
     }
 
     [Header("State Machine")]
@@ -49,6 +54,9 @@ public class PizzaManager : MonoBehaviour
     [SerializeField] private SSO_ScoreData ScoreData;
     [SerializeField] private RSE_RefreshUI RefreshUI;
     [SerializeField] private RSO_Score Score;
+
+    [Header("Trays Visuals")]
+    [SerializeField] private List<IngredientTray> Trays;
 
     private List<Image> _spawnedOrderIcons = new List<Image>();
 
@@ -139,6 +147,8 @@ public class PizzaManager : MonoBehaviour
             _selectedIngredient = pressedIngredient;
             _currentIngredientTurns = 0;
 
+            FillTray(pressedIngredient);
+
             Debug.Log($"Sélection correcte : {pressedIngredient.IngredientName}. Tourne le joystick !");
             TurnText.text = $"Étale {pressedIngredient.IngredientName} (0/{TurnsToPlaceIngredient})";
         } else
@@ -166,6 +176,8 @@ public class PizzaManager : MonoBehaviour
                 Image completedIcon = _spawnedOrderIcons[_currentRecipeIndex];
                 completedIcon.color = new Color(0.3f, 0.3f, 0.3f, 0.5f);
             }
+
+            EmptyTray(_selectedIngredient);
 
             _selectedIngredient = null;
             _currentRecipeIndex++;
@@ -246,6 +258,14 @@ public class PizzaManager : MonoBehaviour
             }
         }
 
+        foreach (var tray in Trays)
+        {
+            if (tray.FilledVisual != null)
+            {
+                tray.FilledVisual.SetActive(false);
+            }
+        }
+
         _turnCount = 0;
         _currentRecipeIndex = 0;
         _currentIngredientTurns = 0;
@@ -309,5 +329,29 @@ public class PizzaManager : MonoBehaviour
         RefreshUI.Raise();
 
         ResetForNextOrder();
+    }
+
+    private void FillTray(SSO_Ingredient ingredient)
+    {
+        foreach (var tray in Trays)
+        {
+            if (tray.Ingredient == ingredient && tray.FilledVisual != null)
+            {
+                tray.FilledVisual.SetActive(true);
+                break;
+            }
+        }
+    }
+
+    private void EmptyTray(SSO_Ingredient ingredient)
+    {
+        foreach (var tray in Trays)
+        {
+            if (tray.Ingredient == ingredient && tray.FilledVisual != null)
+            {
+                tray.FilledVisual.SetActive(false);
+                break;
+            }
+        }
     }
 }
