7e5e161f2b24df49cd05af174513f5099ee671fe
Romain Unternaehrer <r.unternaehrer@rubika-edu.com>
2026-09-24 13:09:23 +0200

    Auto Switch game modes

--- Assets/Scripts/GameModeSwitcher.cs
+++ Assets/Scripts/GameModeSwitcher.cs
@@ -1,5 +1,7 @@
+using Sirenix.OdinInspector;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class GameModeSwitcher : MonoBehaviour
 {
@@ -10,6 +12,15 @@ public class GameModeSwitcher : MonoBehaviour
     [SerializeField] private GameObject RTPizza;
     [SerializeField] private GameObject RTRythm;
     [SerializeField] private GameObject CommandsPanel;
+    [SerializeField] private Slider AutoSwitchSlider;
+
+    [Header("Auto-Switcher")]
+    [SerializeField, SuffixLabel("sec")] private float MinSwitchInterval;
+    [SerializeField, SuffixLabel("sec")] private float MaxSwitchInterval;
+    [ShowInInspector] public float SwitchProgress => _timer / _currentSwitchTarget;
+
+    private float _currentSwitchTarget = 15f;
+    private float _timer = 0f;
 
     private void OnEnable() => SwitchInput.action.Enable();
     private void OnDisable() => SwitchInput.action.Disable();
@@ -17,13 +28,28 @@ public class GameModeSwitcher : MonoBehaviour
     private void Start()
     {
         SwitchToRythmMode();
+        SetNewRandomInterval();
     }
 
     private void Update()
     {
+        _timer += Time.deltaTime;
+
+        if (AutoSwitchSlider != null)
+        {
+            AutoSwitchSlider.value = SwitchProgress;
+        }
+
+        if (_timer >= _currentSwitchTarget)
+        {
+            SwitchGameMode();
+            SetNewRandomInterval();
+        }
+
         if (SwitchInput.action.WasPressedThisFrame())
         {
             SwitchGameMode();
+            SetNewRandomInterval();
         } 
     }
 
@@ -56,5 +82,11 @@ public class GameModeSwitcher : MonoBehaviour
         CommandsPanel.SetActive(false);
         RTPizza.SetActive(false);
         RTRythm.SetActive(true);
+    }
+
+    private void SetNewRandomInterval()
+    {
+        _currentSwitchTarget = Random.Range(MinSwitchInterval, MaxSwitchInterval);
+        _timer = 0f;
     }
 }
